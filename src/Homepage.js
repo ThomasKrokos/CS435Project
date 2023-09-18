e367723a4e882406a889f2c4c0e96ff92a9b0b21
Thomas <tommyk101@yahoo.com>
2023-09-18 14:57:28 -0500

    Tommy and Kolby brainstorm

---
+++ src/Homepage.js
@@ -0,0 +1,3 @@
+// Add step counter in here
+// this will be the first page they see
+// with navigation to the other pages
---
+++ src/WorkoutGenerator.js
@@ -0,0 +1,8 @@
+// look up a workout
+// or have diagram of body that you 
+// can pick a muscle group and it gives workouts
+// or both if we crazy
+
+// Querying chatgpt for workout ideas/example splits
+
+// Videos? or some sort of explanation for workouts
