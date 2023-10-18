22c21f8058e4761120263d527959ad0a18f32994
DTVMAN01 <danielthvollmer@gmail.com>
2023-10-18 12:22:56 -0500

    yo wats up

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+//hello this is patrick
 export default function App() {
   return (
     <View style={styles.container}>
