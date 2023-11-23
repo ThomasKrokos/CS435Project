e112293f44e57fc3e9b161cba7ee6702ef4f4d9c
Thomas Krokos <tommyk101@yahoo.com>
2023-11-23 00:12:59 -0600

    Created form for profile creation minus gender

--- src/Profile.js
+++ src/Profile.js
@@ -1,16 +1,26 @@
 import {
-    Image,
-    Pressable,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-  } from "react-native";
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  Keyboard,
+  View,
+} from "react-native";
+import { useState, useEffect } from "react";
+
 import { RFValue } from "react-native-responsive-fontsize";
+import { Picker } from "@react-native-picker/picker";
 
 import React from "react";
 
 const Profile = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("Male");
+  const [age, setAge] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+
   return (
     <View
       style={[
@@ -20,39 +30,168 @@ const Profile = ({ navigation }) => {
         },
       ]}
     >
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <View style={{ flex: 5 }}>
-        </View>
-        <Pressable
-          style={({ pressed }) => [
-            {
-              marginRight: "5%",
-              alignItems: "flex-end",
-              justifyContent: "flex-end",
-              flex: 1,
-              backgroundColor: pressed ? "#bbbbbb" : "#f2f2f2",
-              borderRadius: "50px",
-              // #f2f2f2 is default background color
-            },
-          ]}
-          onPress={() => navigation.goBack()}
+      <Pressable
+        style={[
+          styles.container,
+          {
+            flexDirection: "column",
+            margin: "5%",
+          },
+        ]}
+        onPress={Keyboard.dismiss}
+      >
+        <View
+          style={{
+            flex: 0.05,
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          <Image
-            source={require("../assets/close.png")}
+          <View style={{ flex: 8 }}></View>
+          <Pressable
+            style={({ pressed }) => [
+              {
+                alignItems: "flex-end",
+                justifyContent: "flex-end",
+                flex: 1,
+                backgroundColor: pressed ? "#bbbbbb" : "#f2f2f2",
+                // #f2f2f2 is default background color
+              },
+            ]}
+            onPress={() => navigation.goBack()}
+          >
+            <Image
+              source={require("../assets/close.png")}
+              style={{
+                width: "100%",
+                height: "100%",
+                flex: 1,
+                alignItems: "flex-end",
+                justifyContent: "flex-end",
+              }}
+              resizeMode="contain"
+            />
+          </Pressable>
+        </View>
+        <View
+          style={{
+            flex: 0.15,
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View style={[styles.headerLine, { flex: 1, marginBottom: "5%" }]}>
+            <Text style={[styles.header, {}]}>Create Your Profile</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            flex: 0.5,
+            flexDirection: "column",
+            margin: "0%, 10%",
+          }}
+        >
+          <View
             style={{
-              width: "100%",
-              height: "100%",
               flex: 1,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
             }}
-            resizeMode="contain"
-          />
-        </Pressable>
-      </View>
-      <View style={{ flex: 5 }}>
-        <Text style={[styles.header, {
-            
-        }]}>Create Your Profile</Text>
-      </View>
+          >
+            <Text style={[styles.smallText, { flex: 0.25 }]}>Name:</Text>
+            <TextInput
+              style={[styles.textInput, { flex: 0.5 }]}
+              placeholder="Name"
+              onChangeText={(text) => setName(text)}
+            />
+            <View style={{ flex: 0.25 }}></View>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text style={[styles.smallText, { flex: 0.25 }]}>Age:</Text>
+            <TextInput
+              style={[styles.textInput, { flex: 0.25 }]}
+              placeholder="Age"
+              keyboardType="number-pad"
+              onChangeText={(text) => setAge(text)}
+            />
+            <View style={{ flex: 0.5 }}></View>
+          </View>
+
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text style={[styles.smallText, { flex: 0.25 }]}>Weight:</Text>
+            <TextInput
+              style={[styles.textInput, { flex: 0.25 }]}
+              keyboardType="number-pad"
+              onChangeText={(text) => setWeight(text)}
+              placeholder="lbs"
+            />
+            <View style={{ flex: 0.5 }}></View>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text style={[styles.smallText, { flex: 0.25 }]}>Height:</Text>
+            <TextInput
+              style={[styles.textInput, { flex: 0.25 }]}
+              keyboardType="number-pad"
+              onChangeText={(text) => setHeight(text)}
+              placeholder="inches"
+            />
+            <View style={{ flex: 0.5 }}></View>
+          </View>
+
+
+        </View>
+
+        <View style={{ flex: 0.1 }}>
+          <Pressable
+            style={({ pressed }) => [
+              styles.button,
+              {
+                flex: 1,
+                padding: "2%",
+                backgroundColor: pressed ? "#bbbbbb" : "#000000",
+              },
+            ]}
+            onPress={() => navigation.navigate("Homepage")}
+          >
+            <Text
+              style={[
+                styles.buttonTitle,
+                {
+                  fontSize: RFValue(18),
+                  fontWeight: "bold",
+                },
+              ]}
+            >
+              Submit
+            </Text>
+          </Pressable>
+        </View>
+        <View style={{ flex: 0.2 }}></View>
+      </Pressable>
     </View>
   );
 };
@@ -62,11 +201,50 @@ export default Profile;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: "10%",
+    marginTop: "5%",
+    marginBottom: "5%",
   },
   header: {
     textAlign: "center",
     fontWeight: "bold",
-    fontSize: RFValue(32),
+    fontSize: RFValue(28),
+  },
+
+  smallText: {
+    textAlign: "left",
+    justifyContent: "center",
+    fontSize: RFValue(20),
+  },
+
+  textInput: {
+    textAlign: "left",
+    justifyContent: "flex-end",
+    backgroundColor: "#ffffff",
+    margin: "2%",
+    borderStyle: "solid",
+    borderColor: "#A3A3A3",
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: "5%",
+    fontSize: RFValue(16),
+  },
+
+  button: {
+    textAlign: "center",
+    justifyContent: "center",
+    backgroundColor: "#000000",
+    margin: "2%",
+    borderRadius: 20,
+  },
+  buttonTitle: {
+    textAlign: "center",
+    justifyContent: "center",
+    color: "#FFFFFF",
+  },
+  headerLine: {
+    paddingBottom: "5%",
+    borderBottomWidth: 1,
+    borderBottomColor: "#000000",
+    borderBottomStyle: "solid",
   },
 });
